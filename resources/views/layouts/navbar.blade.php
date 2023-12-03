be78f0d4061a0c5478496ec06096d7eeda77f10c
NFalah25 <falahnaufal1@gmail.com>
2023-12-03 14:17:49 +0700

    Logout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -208,9 +208,10 @@
           <i class="fas fa-cog"></i> Settings
         </a>
         <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item has-icon text-danger">
+        <a href="#" class="dropdown-item has-icon text-danger" onclick="event.preventDefault(); getElementById('logout-form').submit()">
           <i class="fas fa-sign-out-alt"></i> Logout
         </a>
+        <form id="logout-form" action="{{route('logout')}}" method="post">@csrf</form>
       </div>
     </li>
   </ul>
