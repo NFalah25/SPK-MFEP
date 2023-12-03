c5fb26ddf3e0e1024859638655d6d357fce809f5
alfarchi <8c.alfan@gmail.com>
2023-12-03 16:58:34 +0700

    [Add] Alternatif

---
+++ resources/views/pages/alternatif.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Main Content -->
+    <section class="section">
+        <div class="section-header">
+            <h1>Data Alternatif</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Alternatif</a></div>
+                <div class="breadcrumb-item"><a href="#">Manajemen</a></div>
+                <div class="breadcrumb-item">Tabel</div>
+            </div>
+        </div>
+        <div class="section-body">
+            <h2 class="section-title">Manajemen Alternatif</h2>
+
+            <div class="row">
+                <div class="col-12">
+                    @include('layouts.alert')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>Daftar Alternatif</h4>
+                            <div class="card-header-action">
+                                <a class="btn btn-icon icon-left btn-primary" href="#">Tambah Alternatif</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-md" id="keluarga">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Nama Alternatif</th>
+                                            <th class="text-right">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data as $key => $item)
+                                            <tr>
+                                                <td>{{ $key + 1 }}
+                                                <td>{{ $item -> alternatif }}</td>
+                                                <td class="text-right">
+                                                    <div class="d-flex justify-content-end">
+                                                        <a href="#"
+                                                            class="btn btn-sm btn-secondary btn-icon ml-2 mr-2 d-flex align-items-center">
+                                                            <span><i class="fas fa-edit"></i></span>&nbsp;Ubah
+                                                        </a>
+                                                        <form action=""
+                                                          method="POST">
+                                                          <input type="hidden" name="_method" value="DELETE">
+                                                          <input type="hidden" name="_token"
+                                                              value="{{ csrf_token() }}">
+                                                          <button class="btn btn-sm btn-danger btn-icon confirm-delete d-flex align-items-center">
+                                                          <span><i class="fas fa-times"></i></span>&nbsp;Hapus</button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('customStyle')
+@endpush
